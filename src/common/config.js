cff110cdfda9501091b8e664803c0fef32cba08c
Kristiyan96 <kristiyan.c@gmail.com>
2019-09-14 18:07:07 +0300

    API url in production

--- src/common/config.js
+++ src/common/config.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:3000/api/";
+export const API_URL = process.env.NODE_ENV === "production" ? "https://daylogs.app/api" : "http://localhost:3000/api/";
 export default API_URL;
